d5a35a14e3b5c5acfe386a7685d19403a28108fe
owais <owais.gosha@gmail.com>
2024-01-14 18:19:45 +0500

    changes

--- resources/views/content/users/user-list.blade.php
+++ resources/views/content/users/user-list.blade.php
@@ -4,44 +4,6 @@
 
 @section('script')
     @include('content.users.Js.user-list-js')
-    <script>
-        $('.contactPermission').on('click', function() {
-            let check = ($(this).is(':checked') ? 1 : 0 )
-            let userId = $(this).data('user-id')
-            formData = { 'check': check , 'id': userId }
-            $.ajax({
-                'url': '{{ route("contact-permission") }}',
-                'data': formData,
-                success: function(data) {
-                    if (data == "`success") {
-                        toastr.success('Contact Permission has been Changed Successfully.', 'Permission Changed!');
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error('Ajax request error:', status, error);
-                }
-            })
-        });
-
-        $('.photoPermission').on('click', function() {
-            let check = ($(this).is(':checked') ? 1 : 0 )
-            let userId = $(this).data('user-id')
-            formData = { 'check': check , 'id': userId }
-            $.ajax({
-                'url': '{{ route("photo-permission") }}',
-                'data': formData,
-                success: function(data) {
-                    if (data == "`success") {
-                        toastr.success('Photo Permission has been Changed Successfully.', 'Permission Changed!');
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error('Ajax request error:', status, error);
-                }
-            })
-        });
-
-    </script>
 @endsection
 
 @section('content')
