f5066379854d4fff044a3ac50b00cbd66da5ab43
Hamza Aziz <ha4319151@gmail.com>
2024-01-16 00:16:31 +0500

    property list button style

--- resources/views/content/properties/js/property-list-js.blade.php
+++ resources/views/content/properties/js/property-list-js.blade.php
@@ -1,13 +1,17 @@
 <script>
-    $('#property-table').hide();
+    $('.property-table').hide();
     $(document).ready(function(){
         $('#list').click(function(){
-            $('#property-table').hide();
-            $('#property-list').show();
+            $('.property-table').hide();
+            $('.property-list').show();
+            $('#list').addClass("bg-primary text-white");
+            $('#table').removeClass("bg-primary text-white");
         });
         $('#table').click(function(){
-            $('#property-list').hide();
-            $('#property-table').show();
+            $('.property-list').hide();
+            $('.property-table').show();
+            $('#table').addClass("bg-primary text-white");
+            $('#list').removeClass("bg-primary text-white");
         })
     })
     $('.user-list-table').dataTable();
