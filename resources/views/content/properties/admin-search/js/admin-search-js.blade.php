2d735c2cfd9fe26dd4e85cc0a560b7ff6c3b64e0
sherazaleem <16bscs19138@gmail.com>
2024-01-27 17:20:22 +0500

    update

--- resources/views/content/properties/admin-search/js/admin-search-js.blade.php
+++ resources/views/content/properties/admin-search/js/admin-search-js.blade.php
@@ -12,6 +12,8 @@
 <script>
     $(document).ready(function() {
     $("#myForm").on("submit", function(e) {
+        $(".saveCustomer_processing").removeClass('d-none')
+        $(".saveCustomers_sve_btn").addClass('d-none')
         $('#fetchProperty').html(''); 
         e.preventDefault();
         var formData = $(this).serialize();
@@ -20,6 +22,10 @@
             url: '/admin-ajax-search/',
             data: formData,
             success: function(result) {
+                $(".saveCustomers_sve_btn").css('display', 'block')
+             $(".saveCustomer_processing").css('display', 'none')
+             $(".saveCustomer_processing").addClass('d-none')
+             $(".saveCustomers_sve_btn").removeClass('d-none')
                 $('#fetchProperty').html(result)  
                 }
         });
